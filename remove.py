aa9e22de3b49877725809935997a4ff82e9df449
ShenghuiXue <shenghuixue@gmail.com>
2022-12-14 17:33:23 -0600

    automate batch background remove

--- remove.py
+++ remove.py
@@ -1,16 +1,25 @@
 from rembg import remove
 from PIL import Image
+import os
 
-# define which image need to remove the background
-input_path = 'a2.jpg'
-# define where the new image is going to be saved
-output_path = 'a2out.png'
+# get current working directory
+cwd = os.getcwd()
+# setup input and output directory
+inputDir = cwd + "/input/"
+outputDir = cwd + "/output/"
 
-# open the input image
-picture = Image.open(input_path)
+files = os.listdir(inputDir)
 
-# remove background from the old image
-output = remove(picture)
+for fileName in files:
+	# setup input file path
+	input_path = inputDir + fileName
+	# setup output file path
+	output_path = outputDir + fileName.split('.')[0] + '.png'
 
-# done and save the new image which have the background already removed
-output.save(output_path, format='PNG', quality=95)
+	# open the input image
+	picture = Image.open(input_path)
+	# remove background from the old image
+	output = remove(picture)
+
+	# done and save the new image which have the background already removed
+	output.save(output_path, format='PNG', quality=95)
